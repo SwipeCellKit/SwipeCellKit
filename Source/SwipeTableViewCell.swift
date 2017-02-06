04688dae483551473c425d5a4a2501f4e4a87d5c
Jer K <jerkoch@gmail.com>
2017-02-06 14:26:19 +0100

    Fix bug where default implementation was not exposed as public.
    Additionally, moved the delegate to its own file.

--- Source/SwipeTableViewCell.swift
+++ Source/SwipeTableViewCell.swift
@@ -7,44 +7,7 @@
 
 import UIKit
 
-/**
- The `SwipeTableViewCellDelegate` protocol is adopted by an object that manages the display of action buttons when the cell is swiped.
- */
-public protocol SwipeTableViewCellDelegate: class {
-    /**
-     Asks the delegate for the actions to display in response to a swipe in the specified row.
-     
-     - parameter tableView: The table view object which owns the cell requesting this information.
-     
-     - parameter indexPath: The index path of the row.
-     
-     - parameter orientation: The side of the cell requesting this information.
-     
-     - returns: An array of `SwipeAction` objects representing the actions for the row. Each action you provide is used to create a button that the user can tap.
-    */
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]
-    
-    /**
-     Asks the delegate for the display options to be used while presenting the action buttons.
-     
-     - parameter tableView: The table view object which owns the cell requesting this information.
-     
-     - parameter indexPath: The index path of the row.
-     
-     - parameter orientation: The side of the cell requesting this information.
-     
-     - returns: A `SwipeTableOptions` instance which configures the behavior of the action buttons.
-     
-     - note: If not implemented, a default `SwipeTableOptions` instance is used.
-    */
-    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions
-}
 
-extension SwipeTableViewCellDelegate {
-    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
-        return SwipeTableOptions()
-    }
-}
 
 /**
  The `SwipeTableViewCell` class extends `UITableViewCell` and provides more flexible options for cell swiping behavior.
---
+++ Source/SwipeTableViewCellDelegate.swift
@@ -0,0 +1,50 @@
+//
+//  SwipeTableViewCellDelegate.swift
+//
+//  Created by Jeremy Koch
+//  Copyright © 2017 Jeremy Koch. All rights reserved.
+//
+
+import UIKit
+
+/**
+ The `SwipeTableViewCellDelegate` protocol is adopted by an object that manages the display of action buttons when the cell is swiped.
+ */
+public protocol SwipeTableViewCellDelegate: class {
+    /**
+     Asks the delegate for the actions to display in response to a swipe in the specified row.
+     
+     - parameter tableView: The table view object which owns the cell requesting this information.
+     
+     - parameter indexPath: The index path of the row.
+     
+     - parameter orientation: The side of the cell requesting this information.
+     
+     - returns: An array of `SwipeAction` objects representing the actions for the row. Each action you provide is used to create a button that the user can tap.
+     */
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]
+    
+    /**
+     Asks the delegate for the display options to be used while presenting the action buttons.
+     
+     - parameter tableView: The table view object which owns the cell requesting this information.
+     
+     - parameter indexPath: The index path of the row.
+     
+     - parameter orientation: The side of the cell requesting this information.
+     
+     - returns: A `SwipeTableOptions` instance which configures the behavior of the action buttons.
+     
+     - note: If not implemented, a default `SwipeTableOptions` instance is used.
+     */
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions
+}
+
+/**
+ Default implementation of `SwipeTableViewCellDelegate` methods
+ */
+public extension SwipeTableViewCellDelegate {
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
+        return SwipeTableOptions()
+    }
+}
