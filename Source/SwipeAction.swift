c45bd9d79384e02d35a2f1ae5564b662f587f10a
Marcin Butanowicz <m.butan@gmail.com>
2017-06-13 14:24:35 +0200

    Added highlighted background color for action button

--- Source/SwipeAction.swift
+++ Source/SwipeAction.swift
@@ -63,6 +63,11 @@ public class SwipeAction: NSObject {
     ///
     /// - note: Use this property to specify the background color for your button. If you do not specify a value for this property, the framework assigns a default color based on the value in the style property.
     public var backgroundColor: UIColor?
+  
+    /// The highlighted background color of the action button.
+    ///
+    /// - note: Use this property to specify the highlighted background color for your button.
+    public var highlightedBackgroundColor: UIColor?
     
     /// The visual effect to apply to the action button.
     ///
