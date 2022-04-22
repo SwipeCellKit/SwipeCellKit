0a75b06ddae622cb2ebb36a78382afbe3ddaf932
quanlh <quanlh@tokyotechlab.com>
2022-04-22 13:39:33 +0700

    fix add "weak" to delegate, dataSource of SwipeAction

--- Source/SwipeAction.swift
+++ Source/SwipeAction.swift
@@ -30,8 +30,8 @@ public enum SwipeActionStyle: Int {
  This class lets you define one or more custom actions to display for a given item in your table/collection. Each instance of this class represents a single action to perform and includes the text, formatting information, and behavior for the corresponding button.
  */
 public class SwipeAction: NSObject {
-    var delegate: SwipeActionDelegate?
-    var dataSource: SwipeActionDataSource?
+    weak var delegate: SwipeActionDelegate?
+    weak var dataSource: SwipeActionDataSource?
     
     /// An optional unique action identifier.
     public var identifier: String?
